00e160a08e35c64d7d60d12eb71b7beab3d5eec3
yasinabdmahmood <abd181103@gmail.com>
2023-07-01 02:54:17 +0300

    Fix typo

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.sesstion_store :cookie_store, key: "_authentication_app", domain: 'http://localhost:3001'
+Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: 'http://localhost:3001'
