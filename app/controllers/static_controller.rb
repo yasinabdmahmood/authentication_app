676cd39aae60096dbf759394b47dc44578d9bdf3
yasinabdmahmood <abd181103@gmail.com>
2023-07-01 15:43:53 +0300

    Add index action to static controller to display some data for logged in user

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,4 +2,11 @@ class StaticController < ApplicationController
   def home
     render json: {status: 'It is working'}
   end
+
+  def index
+    data = DummyDatum.all
+    render json: {
+      data: data
+    }
+  end
 end
