25a5821bf50d3bd3b1c1909fcac309fef48a57df
yasinabdmahmood <abd181103@gmail.com>
2023-07-01 15:41:37 +0300

    Add code to session.rb to work in both production and development mode

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: 'http://localhost:3001'
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: 'http://localhost:3001'
+else
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
