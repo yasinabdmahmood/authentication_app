e6f16799c932082beb034efaedb0f30ea6281aaf
yasinabdmahmood <abd181103@gmail.com>
2023-07-01 13:20:14 +0300

    Add sessions controller with implementing create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'static/home'
+  resources :sessions, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/migrate/20230701002007_fix_typo_in_email_field_of_user_model.rb
+++
@@ -1,5 +0,0 @@
-class FixTypoInEmailFieldOfUserModel < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :users, :eamil, :email
-  end
-end
