67cc8f619d96baf4977d4db605571b9a9346425f
yasinabdmahmood <abd181103@gmail.com>
2023-07-01 14:59:18 +0300

    Add two more routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'static/home'
   resources :sessions, only: [:create]
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
